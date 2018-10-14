917e99c5404928caee394821c1e862d574d04c9d
KrystianGrabowski <krystian987543@gmail.com>
2018-10-14 17:12:01 +0200

    triangle, exceptions

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,15 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+    tab = [a, b, c].sort
+    raise TriangleError if tab[2] >= tab[1] + tab[0] or tab[0] <= 0
+    if tab[0] == tab[1] and tab[1] == tab[2]
+        return :equilateral
+    elsif tab[0] == tab[1] or tab[1] == tab[2]
+        return :isosceles
+    else
+        return :scalene
+    end
 end
 
 # Error class used in part 2.  No need to change this code.
